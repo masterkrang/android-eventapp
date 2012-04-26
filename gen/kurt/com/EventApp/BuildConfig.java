9da3d9037f5c1ce3fb3c2c476758f81259cd5725
Kurt Braget (sword computer) <kurtbraget@gmail.com>
2012-04-25 19:49:48 -0700

    initial android project commit

---
+++ gen/kurt/com/EventApp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package kurt.com.EventApp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/kurt/com/EventApp/EventAppActivity.java
@@ -0,0 +1,13 @@
+package kurt.com.EventApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EventAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
