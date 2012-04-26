126374c2638a865b3a5f05d61a6db922c9d76bfc
Kurt Braget (sword computer) <kurtbraget@gmail.com>
2012-04-25 20:21:29 -0700

    testing git

--- src/kurt/com/EventApp/EventAppActivity.java
+++ src/kurt/com/EventApp/EventAppActivity.java
@@ -9,5 +9,8 @@ public class EventAppActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        //testing git
     }
 }
